2f90d6a885fa6c3cb5b62c353c488d762097a061
adamiaka <adamiak.lublin@gmail.com>
2024-03-13 08:09:11 +0100

    version update

--- cypress/e2e/access.spec.cy.js
+++ cypress/e2e/access.spec.cy.js
@@ -2,7 +2,7 @@
 
 import Access from "../support/pageObject/access"
 
-//Login and logout using credentials provided in environmental variables in cyress.config.js
+//Login and logout using credentials provided in environmental variables in cypress.config.js
 describe("Access", () => {
     it("Login and logout", () => {
         Access.logIn(Cypress.env('login'), Cypress.env('password'))
